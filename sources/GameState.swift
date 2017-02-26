4f2887e60e7616a292197aaccd90f911d52f9575
Rasie1 <rasielll@gmail.com>
2017-02-26 19:59:09 +0300

    Implement some classes

--- sources/GameState.swift
+++ sources/GameState.swift
@@ -1,7 +1,7 @@
 struct GameState {
     var players: [PlayerState]
 
-    public init(houses: Int, seeds: Int, players: Int) {
+public init(houses: Int, seeds: Int, players: Int) {
         self.players = [PlayerState](repeating: PlayerState(houses: houses, seeds: seeds),
                                      count: players)
     }
